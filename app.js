b5ca80360f4bbf4b4385ed8a63f053f60e34b5e6
himprover <hwang.improver@gmail.com>
2022-06-15 23:14:40 +0900

    feat: make router directory

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
+require('dotenv').config();
+const { client } = require('./utils/postgres');
 const app = express();
+
+const router = require('./routers/index');
 
 // 포트번호 5000으로 실행
 app.set('port', 5000);
 
-app.get('/', (req, res) => {
-	res.send('기본 / response');
-});
-
 app.listen(app.get('port'), () => {
 	console.log(app.get('port'), '번 포트에서 listen 시작');
 });
+
+app.use(router);
