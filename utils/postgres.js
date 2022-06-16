12871b284ca250a7ecaf416400ce9de2c54f87b3
himprover <hwang.improver@gmail.com>
2022-06-16 19:21:45 +0900

    feat: accessToken, refreshToken get&verify
    1. accessToken 발급, 검증 가능
    2. refreshToken 발급, 검증 가능

--- utils/postgres.js
+++ utils/postgres.js
@@ -1,20 +1,20 @@
-const { Client } = require('pg');
-const Query = require('pg').Query;
+const { Pool } = require('pg');
 
-const client = new Client({
+const config = {
 	user: process.env.DB_USER,
 	host: process.env.DB_HOST,
 	database: process.env.DB_NAME,
 	password: process.env.DB_PASSWORD,
 	port: process.env.DB_PORT,
-});
+};
 
-client.connect((err) => {
-	if (err) {
-		console.error('error', err.stack);
-	} else {
-		console.log('success');
+const pool = new Pool(config);
+
+const isArrayEmpty = (array) => {
+	if (Array.isArray(array) && array.length === 0) {
+		return true;
 	}
-});
+	return false;
+};
 
-module.exports = { client, Query };
+module.exports = { pool, isArrayEmpty };
