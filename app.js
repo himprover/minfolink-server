69aa7da2b7aa1e8fc296201a09097b240d1a0e6d
himprover <hwang.improver@gmail.com>
2022-06-25 19:50:48 +0900

    feat: add cors

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 require('dotenv').config();
 const app = express();
 
+const cors = require('cors');
+
 const router = require('./routers/index');
 
 // 포트번호 5000으로 실행
@@ -12,4 +14,5 @@ app.listen(app.get('port'), () => {
 });
 
 app.use(express.json());
+app.use(cors());
 app.use(router);
