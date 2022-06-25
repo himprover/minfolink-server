cc812592d354ebf27124404d678a9b30d8a080af
himprover <hwang.improver@gmail.com>
2022-06-25 15:04:17 +0900

    feat: add value empty check function

--- utils/postgres.js
+++ utils/postgres.js
@@ -17,4 +17,11 @@ const isArrayEmpty = (array) => {
 	return false;
 };
 
-module.exports = { pool, isArrayEmpty };
+const isValueEmpty = (value) => {
+	if (value === '' || value === undefined || value === null) {
+		return true;
+	}
+	return false;
+};
+
+module.exports = { pool, isArrayEmpty, isValueEmpty };
