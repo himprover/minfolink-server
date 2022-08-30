d1f1491ac9d5cea5315589e5166c64a94f524b67
himprover <hwang.improver@gmail.com>
2022-08-30 21:02:01 +0900

    feat: profile sns link modify API
    1. 프로필 sns 링크 변경하는 API 개발 완료

--- routers/profile/setting/index.js
+++ routers/profile/setting/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 router.use('/link', require('./link'));
 router.use('/nickname', require('./nickname'));
 router.use('/image', require('./image'));
+router.use('/sns', require('./sns'));
 
 module.exports = router;
