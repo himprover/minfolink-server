4bcf4335bc7cc7dd4475afe5d6f8fee0ab445921
himprover <hwang.improver@gmail.com>
2022-08-30 16:04:03 +0900

    feat: profile image file filter
    1. 프로필 이미지 필터 적용 (jpg, jpeg, png, gif 파일만 가능)

--- routers/profile/setting/image/index.js
+++ routers/profile/setting/image/index.js
@@ -9,13 +9,35 @@ const {
 const { pool } = require('../../../../utils/postgres');
 const { s3_storage } = require('../../../../utils/s3');
 
+const uploadProfileImageFilter = (req, file, cb) => {
+	const type = file.mimetype.split('/')[1];
+	switch (type) {
+		case 'jpg':
+		case 'jpeg':
+		case 'png':
+		case 'gif':
+			req.fileValidateError = false;
+			cb(null, true);
+			break;
+		default:
+			req.fileValidateError = true;
+			cb(null, false);
+	}
+};
+
 const s3_upload = multer({
 	storage: s3_storage('user/profile/image/'),
+	fileFilter: uploadProfileImageFilter,
+	limits: { fileSize: 10 * 1024 * 1024 },
 });
 
 const upload_profile_s3 = s3_upload.fields([{ name: 'image', maxCount: 1 }]);
 
 router.patch('/', upload_profile_s3, async (req, res) => {
+	if (req.fileValidateError) {
+		return res.status(400).end();
+	}
+
 	const { id } = verifyAccessToken(getAccessTokenInRequest(req));
 
 	const newImageUrl = req.files['image'][0].location;
