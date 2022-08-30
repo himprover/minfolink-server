debaac1adc68cb337e3e3c7a7652da487e6d2ec1
himprover <hwang.improver@gmail.com>
2022-08-30 14:22:43 +0900

    feat: upload file to s3 bucket
    1. s3 버킷에 업로드 하는 함수

---
+++ utils/s3.js
@@ -0,0 +1,32 @@
+const aws = require('aws-sdk');
+const multer = require('multer');
+const multer_s3 = require('multer-s3');
+const iconv = require('iconv-lite');
+
+const s3 = new aws.S3({
+	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+	region: process.env.AWS_REGION,
+});
+
+const storage = (src) =>
+	multer_s3({
+		s3: s3,
+		bucket: 'minfolink',
+		contentType: multer_s3.AUTO_CONTENT_TYPE,
+		acl: 'public-read',
+		metadata: function (req, file, cb) {
+			cb(null, { fieldName: file.fieldname });
+		},
+		key: function (req, file, cb) {
+			file.originalname = iconv.decode(file.originalname, 'utf-8');
+			cb(null, `${src}${Date.now()}_${file.originalname}`);
+		},
+	});
+
+const s3_upload = (src) =>
+	multer({
+		storage: storage(src),
+	});
+
+module.exports = { s3_upload };
